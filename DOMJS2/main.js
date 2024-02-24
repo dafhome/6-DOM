46ad0d32924bc500e79623e04d2be24fddd4a559
dafhome <d.fernalvarez@gmail.com>
2024-02-24 15:11:40 +0100

    commit

--- DOMJS2/main.js
+++ DOMJS2/main.js
@@ -1,2 +1,16 @@
 // 2- RELLENAR TICKET DE CINE
 // Tienes una imagen de ticket de entrada para el cine, vacío, y unos inputs para rellenar: se te pide el nombre, fecha y la butaca (fila y asiento). Y al darle botón 'enviar', que se muestren en la imagen del ticket de entrada las opciones elegidas por el usuario. Esto implica maquetar el ticket mediante divs y CSS (puedes usar también una imagen de fondo para el papel).
+
+function datosEntrada (){
+    let a = document.getElementById("nombre").value;
+    let b = document.getElementById("apellidos").value;
+    let c = document.getElementById("email").value;
+    let d = document.getElementById("fecha").value;
+    let e = document.getElementById("hora").value;
+    let f = document.getElementById("fila").value;
+    let g = document.getElementById("asiento").value;
+
+
+
+    
+}
